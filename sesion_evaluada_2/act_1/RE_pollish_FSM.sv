8543812a92ef1c5efbd52f1ed6b7ac6deb9aee3c
jcatala <jose.catalanf@sansano.usm.cl>
2021-06-24 04:43:53 -0400

    adding sesion evaluada 7

---
+++ sesion_evaluada_2/act_1/RE_pollish_FSM.sv
@@ -0,0 +1,124 @@
+`timescale 1ns / 1ps
+
+
+module RE_pollish_FSM #(parameter WIDTH = 16)(
+    input logic     clk,
+    input logic     reset,
+    input logic     DataDebouncedIn,
+
+    output logic    out_LoadOpA,
+    output logic    out_LoadOpB,
+    output logic    out_LoadOpCode,
+    output logic    out_updateRes,
+    output logic    out_ToDisplaySel,
+    output logic    [2:0]out_Status
+    );
+
+
+    enum logic[7:0] {Entering_OpA,
+    Load_OpA,
+    Entering_OpB,
+    Load_OpB,
+    Entering_OpCode,
+    Load_OpCode,
+    Show_Result} state, next_state;
+
+
+/*
+Flip flop logic
+*/
+always_ff @( posedge clk ) begin : clk_things
+    if (reset)  state <=  Load_OpA;
+    else        state <= next_state;
+end
+
+/*
+Logic for state change
+*/
+always_comb begin
+    next_state = state;
+    case (state)
+        Entering_OpA:
+        begin
+            if (DataDebouncedIn) next_state = Load_OpA;
+        end 
+        Load_OpA:
+        begin
+            next_state = Entering_OpB;
+        end
+        Load_OpB:
+        begin
+            if (DataDebouncedIn) next_state = Entering_OpCode;
+        end
+        Entering_OpB:
+        begin
+            next_state = Entering_OpCode;
+        end
+        Entering_OpCode:
+        begin
+            if(DataDebouncedIn) next_state = Load_OpCode;
+        end
+        Load_OpCode:
+        begin
+            next_state = Show_Result;
+        end
+        Show_Result:
+        begin
+            next_state = Entering_OpA;
+        end
+        default: 
+        begin
+            next_state = next_state;
+        end
+    endcase
+end
+
+/*
+Logic for output management
+*/
+always_comb begin
+    // default assignments
+    out_LoadOpA           = 1'b0;
+    out_LoadOpB           = 1'b0;
+    out_LoadOpCode        = 1'b0;
+    out_updateRes          = 1'b0;
+    out_ToDisplaySel       = 1'b0;
+
+    case (state)
+        Entering_OpA:
+        begin
+            out_Status      = 0;
+        end
+        Load_OpA:
+        begin
+            out_LoadOpA     = 1'b1;
+            out_Status      = 1;
+        end
+        Entering_OpB:
+        begin
+            out_Status      = 2;
+        end
+        Load_OpB:
+        begin
+            out_LoadOpB     = 1'b1;
+            out_Status      = 3;
+        end
+        Entering_OpCode:
+        begin
+            out_Status      = 4;
+        end
+        Load_OpCode:
+        begin
+            out_LoadOpCode  = 1'b1;
+            out_Status      = 5;
+        end
+        Show_Result:
+        begin
+            out_ToDisplaySel = 1'b1;
+            out_Status      = 6;
+        end
+    endcase
+end
+
+
+endmodule
---
+++ sesion_evaluada_2/act_1/S7_actividad1.sv
@@ -0,0 +1,123 @@
+`timescale 1ns / 1ps
+
+
+module S7_actividad1 #(parameter N_DEBOUNCER = 10)(
+    input logic             clk,
+    input logic             resetN, // To debounce signal
+    input logic             Enter,
+    input logic [15:0]      DataIn, // To alu
+
+    output logic [15:0]     ToDisplay,
+    output logic [3:0]      Flags,
+    output logic [2:0]      Status
+    );
+
+	logic PB_pressed_status;   // clean and synchronized pulse for button pressed
+	logic PB_pressed_pulse;    // high if button is pressed
+	logic PB_released_pulse;    // clean and synchronized pulse for button released
+
+    localparam width = 16;
+
+    // Debounce the resetN input
+    debouncer_FSM #(.DELAY(15)) dummy_debouncer (
+        .clk(clk),
+        .rst(resetN),
+        .PB(Enter),
+        .PB_pressed_status(PB_pressed_status),
+        .PB_pressed_pulse(PB_pressed_pulse),
+        .PB_released_pulse(PB_released_pulse)
+    );
+
+    // Inputs to the ALU 
+    logic toalu_loadopA;
+    logic toalu_loadopB;
+    logic toalu_loadopOp;
+
+    logic toDisplaySel;
+    
+    // Enter the clean input to the pollish
+    // updateRes must go to the ALU result register
+    // ToDisplaySel to the display selector
+    // Status to the status output 
+    RE_pollish_FSM #(.WIDTH(width)) dummy_pollish_fsm (
+        .DataDebouncedIn(PB_released_pulse),
+        .clk(clk),
+        .reset(resetN),
+        .out_LoadOpA(toalu_loadopA),
+        .out_LoadOpB(toalu_loadopB),
+        .out_LoadOpCode(toalu_loadopOp),
+
+        .out_Status(Status),
+        .out_ToDisplaySel(toDisplaySel),
+        .out_updateRes(afteralu_updateRes)
+    );
+
+    // Create each register for the ALU
+    // A, B, OpCode
+    // Registro para salida de la ALU
+    logic [width:0] out_reg_A;
+    logic [width:0] out_reg_B;
+    logic [1:0] out_reg_Op;
+    registro #(.N(width)) reg_A (
+        .clk(clk),
+        .reset(resetN),
+        .load(toalu_loadopA),
+        .In(DataIn),
+        .Out(out_reg_A)
+    );
+
+    registro #(.N(width)) reg_B (
+        .clk(clk),
+        .reset(resetN),
+        .load(toalu_loadopB),
+        .In(DataIn),
+        .Out(out_reg_B)
+    );
+
+    registro #(.N(2)) reg_Op (
+        .clk(clk),
+        .reset(resetN),
+        .load(toalu_loadopB),
+        .In(DataIn),
+        .Out(out_reg_Op)
+    );
+
+
+    // Instanciate the alu
+    ALU_ref #(.C_WIDTH(width)) dummy_alu_ref(
+        .A(out_reg_A),
+        .B(out_reg_B),
+        .OpCode(out_reg_Op),
+        .Result(alu_result),
+        .Status(alu_flags)
+    );
+
+    // Output of alu must go in to a register, so create the cable
+    logic [width-1:0] alu_result;
+    logic [3:0] alu_flags;
+    logic afteralu_updateRes;
+    logic afteralu_Result;
+    logic to_displaySelector;
+
+    registro #(.N(width)) reg_result (
+        .clk(clk),
+        .reset(resetN),
+        .load(afteralu_updateRes),
+        .In(alu_result),
+        .Out(to_displaySelector)
+    );
+
+    
+    registro #(.N(4)) reg_flags (
+        .clk(clk),
+        .reset(resetN),
+        .load(afteralu_updateRes),
+        .In(alu_flags),
+        .Out(Flags)
+    );
+
+
+
+
+endmodule
+
